43175ee3d68dcd9e99719e18af3c250f16cda5e4
takaki.ishikawa <takaki.ishikawa@suns-MacBook-Air-9.local>
2026-04-25 10:16:10 +0700

    Revert "chore: Tech stack compliance to v2.0 policy (MetaGo自動修正) (#7)"
    This reverts commit 371213cf8f5ba2e1127b0c020c85beb605844cab.

--- src/templates/dashboard-01/chart-area.tsx
+++ src/templates/dashboard-01/chart-area.tsx
@@ -1,30 +1,7 @@
 "use client";
 
 import * as React from "react";
-import dynamic from "next/dynamic";
-
-const AreaChart = dynamic(
-  () => import("recharts").then((m) => ({ default: m.AreaChart })),
-  {
-    ssr: false,
-    loading: () => <div className="animate-pulse h-40 bg-muted rounded" />,
-  },
-);
-const Area = dynamic(
-  () => import("recharts").then((m) => ({ default: m.Area })),
-  {
-    ssr: false,
-    loading: () => <div className="animate-pulse h-40 bg-muted rounded" />,
-  },
-);
-const CartesianGrid = dynamic(
-  () => import("recharts").then((m) => ({ default: m.CartesianGrid })),
-  { ssr: false },
-);
-const XAxis = dynamic(
-  () => import("recharts").then((m) => ({ default: m.XAxis })),
-  { ssr: false },
-);
+import { Area, AreaChart, CartesianGrid, XAxis } from "recharts";
 
 import {
   Card,
